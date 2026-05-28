f8db25acd76b63010b03d468cb69cb3f38a00e0c
Carlos Henrique Feijó Luz <carlosluz@MacBook-Air-de-Carlos.local>
2026-05-28 14:30:52 -0300

    feat: analises do vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Oswald, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/next";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -38,6 +39,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
